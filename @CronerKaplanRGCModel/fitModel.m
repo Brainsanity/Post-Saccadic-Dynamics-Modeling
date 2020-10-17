c442103d77a99ca4e5a7289c46d240ae4235b007
Brainsanity@Desktop <yangbin1004@gmail.com>
2020-10-17 10:20:12 -0400

    Update msg print for CronerKaplanRGCModel/fitModel

--- @CronerKaplanRGCModel/fitModel.m
+++ @CronerKaplanRGCModel/fitModel.m
@@ -24,21 +24,25 @@ function fitRawData(obj)
     minY = [0.0044, 11.6141*0.0044, eps; 0.0112, 38.8495*0.0112, eps];     % minimal radius as half the minimal cell spacing
 
     for( k = 1 : 2 )
+        fprintf('%s Cell - Fitting Raw Data: ecc => center radius...\n', Cell{k});
         x = obj.([Cell{k} 'CenterData'])('size').eccDegs;
         y = obj.([Cell{k} 'CenterData'])('size').radiusDegs;
-        [obj.([Cell{k} 'CenterRadiusFunction']), obj.([Cell{k} 'CenterRadiusParams']), obj.([Cell{k} 'CenterRadiusParamsSE'])] = nonLinearFitData(x, y, [], [], obj.([Cell{k} 'CenterData'])('size').initialParams, obj.fitIntercept, minY(k,1));
+        [obj.([Cell{k} 'CenterRadiusFunction']), obj.([Cell{k} 'CenterRadiusParams']), obj.([Cell{k} 'CenterRadiusParamsSE']), isConverged] = nonLinearFitData(x, y, [], [], obj.([Cell{k} 'CenterData'])('size').initialParams, obj.fitIntercept, minY(k,1));
 
+        fprintf('%s Cell - Fitting Raw Data: ecc => surround radius...\n', Cell{k});
         x = obj.([Cell{k} 'SurroundData'])('size').eccDegs;
         y = obj.([Cell{k} 'SurroundData'])('size').radiusDegs;
-        [obj.([Cell{k} 'SurroundRadiusFunction']), obj.([Cell{k} 'SurroundRadiusParams']), obj.([Cell{k} 'SurroundRadiusParamsSE'])] = nonLinearFitData(x, y, [], [], obj.([Cell{k} 'SurroundData'])('size').initialParams, obj.fitIntercept, minY(k,2));
+        [obj.([Cell{k} 'SurroundRadiusFunction']), obj.([Cell{k} 'SurroundRadiusParams']), obj.([Cell{k} 'SurroundRadiusParamsSE']), isConverged] = nonLinearFitData(x, y, [], [], obj.([Cell{k} 'SurroundData'])('size').initialParams, obj.fitIntercept, minY(k,2));
         
+        fprintf('%s Cell - Fitting Raw Data: center radius => center peak sensitivity...\n', Cell{k});
         x = obj.([Cell{k} 'CenterData'])('sensitivity').radiusDegs;
         y = obj.([Cell{k} 'CenterData'])('sensitivity').peakSensitivity;
-        [obj.([Cell{k} 'CenterPeakSensitivityFunction']), obj.([Cell{k} 'CenterPeakSensitivityParams']), obj.([Cell{k} 'CenterPeakSensitivityParamsSE'])] = nonLinearFitData(x, y, [], [], obj.([Cell{k} 'CenterData'])('sensitivity').initialParams, obj.fitIntercept, minY(k,3));
+        [obj.([Cell{k} 'CenterPeakSensitivityFunction']), obj.([Cell{k} 'CenterPeakSensitivityParams']), obj.([Cell{k} 'CenterPeakSensitivityParamsSE']), isConverged] = nonLinearFitData(x, y, [], [], obj.([Cell{k} 'CenterData'])('sensitivity').initialParams, obj.fitIntercept, minY(k,3));
 
+        fprintf('%s Cell - Fitting Raw Data: surround radius => surround peak sensitivity...\n', Cell{k});
         x = obj.([Cell{k} 'SurroundData'])('sensitivity').radiusDegs;
         y = obj.([Cell{k} 'SurroundData'])('sensitivity').peakSensitivity;
-        [obj.([Cell{k} 'SurroundPeakSensitivityFunction']), obj.([Cell{k} 'SurroundPeakSensitivityParams']), obj.([Cell{k} 'SurroundPeakSensitivityParamsSE'])] = nonLinearFitData(x, y, [], [], obj.([Cell{k} 'SurroundData'])('sensitivity').initialParams, obj.fitIntercept, minY(k,3));
+        [obj.([Cell{k} 'SurroundPeakSensitivityFunction']), obj.([Cell{k} 'SurroundPeakSensitivityParams']), obj.([Cell{k} 'SurroundPeakSensitivityParamsSE']), isConverged] = nonLinearFitData(x, y, [], [], obj.([Cell{k} 'SurroundData'])('sensitivity').initialParams, obj.fitIntercept, minY(k,3));
     end
 end
 
@@ -48,11 +52,12 @@ function usePaperFits(obj)
     minY = [0.0044, 11.6141*0.0044, eps];     % minimal radius as half the minimal cell spacing
 
     % Fit the ecc - center radius data
+    fprintf('P Cell - Fitting Median Data: ecc => center radius...\n');
     x = obj.PCenterData('size').eccDegsTable;
     yMedian = obj.PCenterData('size').radiusDegsMedianTable;
     yIQR = obj.PCenterData('size').radiusDegsIQRTable;
     ySamplesNum = obj.PCenterData('size').samplesTable;
-    [obj.PCenterRadiusFunction, obj.PCenterRadiusParams, obj.PCenterRadiusParamsSE] = nonLinearFitData(x, yMedian, yIQR, ySamplesNum, obj.PCenterData('size').initialParams, obj.fitIntercept, minY(1));
+    [obj.PCenterRadiusFunction, obj.PCenterRadiusParams, obj.PCenterRadiusParamsSE, isConverged] = nonLinearFitData(x, yMedian, yIQR, ySamplesNum, obj.PCenterData('size').initialParams, obj.fitIntercept, minY(1));
     
     
     % The ecc - surround radius equation from the paper (Figure 4 caption)
@@ -79,39 +84,43 @@ function fitMedianData(obj)
 
     for( k = 1 : 2 )
         % Fit the ecc - center radius data
+        fprintf('%s Cell - Fitting Median Data: ecc => center radius...\n', Cell{k});
         x = obj.([Cell{k} 'CenterData'])('size').eccDegsTable;
         yMedian = obj.([Cell{k} 'CenterData'])('size').radiusDegsMedianTable;
         yIQR = obj.([Cell{k} 'CenterData'])('size').radiusDegsIQRTable;
         ySamplesNum = obj.([Cell{k} 'CenterData'])('size').samplesTable;
-        [obj.([Cell{k} 'CenterRadiusFunction']), obj.([Cell{k} 'CenterRadiusParams']), obj.([Cell{k} 'CenterRadiusParamsSE'])] = nonLinearFitData(x, yMedian, yIQR, ySamplesNum, obj.([Cell{k} 'CenterData'])('size').initialParams, obj.fitIntercept, minY(k,1));
+        [obj.([Cell{k} 'CenterRadiusFunction']), obj.([Cell{k} 'CenterRadiusParams']), obj.([Cell{k} 'CenterRadiusParamsSE']), isConverged] = nonLinearFitData(x, yMedian, yIQR, ySamplesNum, obj.([Cell{k} 'CenterData'])('size').initialParams, obj.fitIntercept, minY(k,1));
         
         % Fit the ecc - surround radius data
+        fprintf('%s Cell - Fitting Median Data: ecc => surround radius...\n', Cell{k});
         x = obj.([Cell{k} 'SurroundData'])('size').eccDegsTable;
         yMedian = obj.([Cell{k} 'SurroundData'])('size').radiusDegsMedianTable;
         yIQR = obj.([Cell{k} 'SurroundData'])('size').radiusDegsIQRTable;
         ySamplesNum = obj.([Cell{k} 'SurroundData'])('size').samplesTable;
-        [obj.([Cell{k} 'SurroundRadiusFunction']), obj.([Cell{k} 'SurroundRadiusParams']), obj.([Cell{k} 'SurroundRadiusParamsSE'])] = nonLinearFitData(x, yMedian, yIQR, ySamplesNum, obj.([Cell{k} 'SurroundData'])('size').initialParams, obj.fitIntercept, minY(k,2));
+        [obj.([Cell{k} 'SurroundRadiusFunction']), obj.([Cell{k} 'SurroundRadiusParams']), obj.([Cell{k} 'SurroundRadiusParamsSE']), isConverged] = nonLinearFitData(x, yMedian, yIQR, ySamplesNum, obj.([Cell{k} 'SurroundData'])('size').initialParams, obj.fitIntercept, minY(k,2));
 
         
         % Fit the radius - center sensitivity data
+        fprintf('%s Cell - Fitting Median Data: center radius => center peak sensitivity...\n', Cell{k});
         x = obj.([Cell{k} 'CenterData'])('size').radiusDegsMedianTable;
         yMedian = obj.([Cell{k} 'CenterData'])('sensitivity').peakSensitivityMedianTable;
         yIQR = obj.([Cell{k} 'CenterData'])('sensitivity').peakSensitivityIQRTable;
         ySamplesNum = obj.([Cell{k} 'CenterData'])('sensitivity').samplesTable;
-        [obj.([Cell{k} 'CenterPeakSensitivityFunction']), obj.([Cell{k} 'CenterPeakSensitivityParams']), obj.([Cell{k} 'CenterPeakSensitivityParamsSE'])] = nonLinearFitData(x, yMedian, yIQR, ySamplesNum, obj.([Cell{k} 'CenterData'])('sensitivity').initialParams, obj.fitIntercept,minY(k,3));
+        [obj.([Cell{k} 'CenterPeakSensitivityFunction']), obj.([Cell{k} 'CenterPeakSensitivityParams']), obj.([Cell{k} 'CenterPeakSensitivityParamsSE']), isConverged] = nonLinearFitData(x, yMedian, yIQR, ySamplesNum, obj.([Cell{k} 'CenterData'])('sensitivity').initialParams, obj.fitIntercept,minY(k,3));
         
         
         % Fit the radius - surround sensitivity data
+        fprintf('%s Cell - Fitting Median Data: surround radius => surround peak sensitivity...\n', Cell{k});
         x = obj.([Cell{k} 'SurroundData'])('size').radiusDegsMedianTable;
         yMedian = obj.([Cell{k} 'SurroundData'])('sensitivity').peakSensitivityMedianTable;
         yIQR = obj.([Cell{k} 'SurroundData'])('sensitivity').peakSensitivityIQRTable;
         ySamplesNum = obj.([Cell{k} 'SurroundData'])('sensitivity').samplesTable;
-        [obj.([Cell{k} 'SurroundPeakSensitivityFunction']), obj.([Cell{k} 'SurroundPeakSensitivityParams']), obj.([Cell{k} 'SurroundPeakSensitivityParamsSE'])] = nonLinearFitData(x, yMedian, yIQR, ySamplesNum, obj.([Cell{k} 'SurroundData'])('sensitivity').initialParams, obj.fitIntercept, minY(k,3));
+        [obj.([Cell{k} 'SurroundPeakSensitivityFunction']), obj.([Cell{k} 'SurroundPeakSensitivityParams']), obj.([Cell{k} 'SurroundPeakSensitivityParamsSE']), isConverged] = nonLinearFitData(x, yMedian, yIQR, ySamplesNum, obj.([Cell{k} 'SurroundData'])('sensitivity').initialParams, obj.fitIntercept, minY(k,3));
     end
 end
 
 
-function [powerFunction, fittedParams, fittedParamsSE] = nonLinearFitData(x, y, yIQR, ySamplesNum, initialParams, fitIntercept, minY)
+function [powerFunction, fittedParams, fittedParamsSE, isConverged] = nonLinearFitData(x, y, yIQR, ySamplesNum, initialParams, fitIntercept, minY)
     
     % Objective Function
     if(fitIntercept)
@@ -143,12 +152,12 @@ function [powerFunction, fittedParams, fittedParamsSE] = nonLinearFitData(x, y, 
             initialParams(1:2+fitIntercept) = fittedParams;
         else
             isConverged = true;
-            fprintf('Converged, itr = %d\n', itr);
+            fprintf('\tFitting converged after %d iterations of nlinfit()\n\n', itr);
             break;
         end
     end
     if(~isConverged)
-        fprintf('Warning: Fitting not converged after %d times of iterations of nlinfit()\n', 100);
+        fprintf('\tWarning: Fitting not converged after %d iterations of nlinfit()\n\n', 100);
     end
 
     % standard error of the mean
