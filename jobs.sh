495ae6941fe4e11425619d16ac2757f80ab9d87f
Brainsanity@Desktop <yangbin1004@gmail.com>
2021-05-10 02:13:27 -0400

    Update

--- jobs.sh
+++ jobs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #SBATCH  -p standard --time=12:00:00 --cpus-per-task=22 --mem=72g
 
-hostname >> ../logs/condition-$1.txt
-
-matlab -nodesktop -nosplash -r "RunSimulation($1, 11.5);" >> ../logs/condition-$1.txt 2>>&1
+for iCond in $*
+do
+	hostname >> ../logs/condition-$iCond.txt
+	matlab -nodesktop -nosplash -r "RunSimulation($iCond, 11.5); exit;" >> ../logs/condition-$iCond.txt 2>>&1
+done
