4e6510466939c1eb2f383bb8abda2da071e22129
Brainsanity@Desktop <yangbin1004@gmail.com>
2021-05-10 01:27:19 -0400

    Update

--- jobtest.sh
+++ jobtest.sh
@@ -8,5 +8,5 @@ echo "$(hostname)="$s
 
 hostname >> ../condition-$1.txt
 
-module load matlab/r2020b
-matlab -nodesktop -nosplash -r "disp(2*$1); disp($s)" >> ../logs/condition-$1.txt 2>>&1
+# module load matlab/r2020b
+# matlab -nodesktop -nosplash -r "disp(2*$1); disp($s)" >> ../logs/condition-$1.txt 2>>&1
