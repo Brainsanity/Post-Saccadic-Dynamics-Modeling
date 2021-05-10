f6c6ea7f4a4768d569b5f3edcdb8a9926010b485
Brainsanity@Desktop <yangbin1004@gmail.com>
2021-05-10 01:15:48 -0400

    Update

--- jobtest.sh
+++ jobtest.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -o job_out_test.$1.txt -t 00:05:00
 
-hostname
-s=`expr hostname`
-echo $s
-s=`expr $hostname`
+s=$(echo `hostname`)
+echo "$(echo `hostname`)="$s
+s=$(hostname)
+echo "$(hostname)="$s
 # module load matlab/r2020b
 # matlab -nodesktop -nosplash -r "disp(2*$1); disp($s)"
