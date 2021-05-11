75ccdc023cb895e68fcd5a3ceb0f8d6187251197
Brainsanity@Desktop <yangbin1004@gmail.com>
2021-05-11 09:30:09 -0400

    Update

--- jobs.sh
+++ jobs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#SBATCH --time=12:00:00 --cpus-per-task=22 --mem=72g --mail-type=all -p standard
+#SBATCH --time=12:00:00 --cpus-per-task=22 --mem=72g --mail-type=all -p interactive
 
 for iCond in $*
 do
 	hostname >> ../logs/condition-$iCond.txt
+	module load matlab/r2020b
 	matlab -nodesktop -nosplash -r "RunSimulation($iCond, 11.5); exit;" >> ../logs/condition-$iCond.txt 2>&1
 done
