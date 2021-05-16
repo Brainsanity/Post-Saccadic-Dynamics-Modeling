d0d1093af3ca274c96ab538e59368f329c548a53
Bin Yang <yichuansen1004@gmail.com>
2021-05-16 19:53:07 -0400

    update

--- RunSimulation.m
+++ RunSimulation.m
@@ -1,11 +1,21 @@
-function RunSimulation(iConds, nHours)
+function RunSimulation(iConds, nHours, stabilize)
 	if(~exist('nHours', 'var') || isempty(nHours))
 		nHours = Inf;
 	end
+	if( ~exist('stabilize', 'var') || isempty(stabilize) )
+		stabilize = 'normal';
+	end
+
 	addpath('../../EMAT-2.1.0.0');
 
 	encoder = Encoder([], true);
 	sbj = 'SacDB';
-	folder = 'UG - Noise & Grating Simulated Separately';
-	encoder.SimulateExampleCellsActivities_SacDB(iConds, '../../Data', [], 'none', folder, nHours);
+	if(strcmpi(stabilize, 'drift'))
+		folder = 'UG - Noise & Grating Simulated Separately - Drift Stabilized';
+	elseif(strcmpi(stabilize, 'saccade'))
+		folder = 'UG - Noise & Grating Simulated Separately - Saccade Stabilized';
+	else
+		folder = 'UG - Noise & Grating Simulated Separately';
+	end
+	encoder.SimulateExampleCellsActivities_SacDB(iConds, '../../Data', [], stabilize, folder, nHours);
 end
